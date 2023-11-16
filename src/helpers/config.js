56b5b2eace653a1e17f4f196aeb0bbd46727988c
Ibrahim <suleimanonnet@gmail.com>
2023-11-16 19:16:56 +0100

    Fix config issue

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,4 @@
-const env = "development"
+const env = process.env.REACT_APP_ENVIRONMENT
 const productionBaseUrl = process.env.REACT_APP_PRODUCTION_BASE_URL
 const developmentBaseUrl = process.env.REACT_APP_DEVELOPMENT_BASE_URL
 
